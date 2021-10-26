e02844a2f2170500fff872b4b87b0f19e1569d90
wesleytchen <wesleychen121@gmail.com>
2021-10-25 20:24:56 -0500

    finished proj

---
+++ Assets/Scripts/MarbleBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleBehavior : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 15f;
+    
+    private float fbInput;
+    private float lrInput;
+    
+    private Rigidbody _rb;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Put code is for movement using the Sprite's native variables here
+        fbInput = Input.GetAxis("Vertical") * moveSpeed;
+        lrInput = Input.GetAxis("Horizontal") * rotateSpeed;
+    }
+    
+    void FixedUpdate()
+    {
+    
+        //Put code that moves the sprite using the RigidBody here
+        Vector3 rotation = Vector3.up * lrInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.forward * fbInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+    
+}
